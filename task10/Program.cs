c71a79f8299a6e57affd989b1b24fe8ce5f2541d
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-06 20:57:03 +0300

    add task 10

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+string num = Console.ReadLine();
+int x = int.Parse(num);
+
+if (x>=100 && x<1000)
+{
+    int a = (x / 10) % 10;
+
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Число не соответствует критериям");
+}
+
+
+
+
